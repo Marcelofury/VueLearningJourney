34eaa95cb1c27e172b6e5a13d9c7b443883741d9
Marcelofury <marcelofury13579@gmail.com>
2026-06-03 14:14:24 +0300

    imported template function for the part1 component

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="app-wrapper">
+    <header class="main-header">
+      <h1>My Coursera Vue.js Learning Journey</h1>
+      <p>Tracking daily progress and pushing modules to Github Chronologically</p>
+      <Part1_CreateComponents/>
+    </header>
+  </main>
+</template>
